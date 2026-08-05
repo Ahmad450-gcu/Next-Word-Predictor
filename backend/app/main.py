d00ed1e79e22407ea29f7cab0fde10e7636340a1
Ahmad450-gcu <ahmadmaqbool4500@gmail.com>
2026-08-05 15:36:19 +0500

    CORS Middleware hardocded in backend main.py for testing, frontend committed

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,7 +7,10 @@ app = FastAPI(title="Next Word Predictor API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.cors_origins_list,
+    allow_origins=[
+        "https://next-word-predictor-iota.vercel.app"
+    ],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
